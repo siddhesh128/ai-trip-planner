d0681d46a5628574068451aba00523b849725c85
siddhesh128 <119682823+siddhesh128@users.noreply.github.com>
2024-07-27 16:15:41 +0530

    Update Footer.jsx

--- src/view-trip/components/Footer.jsx
+++ src/view-trip/components/Footer.jsx
@@ -4,10 +4,10 @@ function Footer() {
   return (
     <div className="my-12 border-t-2 w-full">
       <h2 className="text-center mt-5 text-gray-400">
-        Created by Aditya Dobhada
+        Created by Siddhesh Jadhav
       </h2>
       <h3 className="text-gray-400 text-center">
-        Yatra Sathi - AI Travel Planner App
+    AI Travel Planner App
       </h3>
     </div>
   );
